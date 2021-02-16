00040cb36765929f27691b14477c08c03837d4b6
Sajeed Ahmed <sajeedahmed@Sajeeds-MBP.hsd1.mi.comcast.net>
2021-02-16 00:11:01 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sajeed <code>src/App.js</code> first React app in AWS.
         </p>
         <a
           className="App-link"
